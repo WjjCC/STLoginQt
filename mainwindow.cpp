77b6771b0151985ef6b8ecc89a045824046faffc
wjjhpc <1262420076@qq.com>
2018-05-17 22:43:10 +0800

    modify

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,13 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    QMainWindow(parent)
+
 {
-    ui->setupUi(this);
+    setStyleSheet("border-image:url(:/img/lgbackground.png);");
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+
 }
